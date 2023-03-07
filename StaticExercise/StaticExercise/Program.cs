53ae1eb39395f02d094519c2259a1be05eab1dba
Cschmi22 <119636252+Cschmi22@users.noreply.github.com>
2023-03-07 09:58:02 -0500

    Add files via upload

---
+++ StaticExercise/StaticExercise/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StaticExercise;
+
+Console.WriteLine($"Fahrenheit to Celsius: {TempConverter.FahrenheitToCelsius(32)}");
+
+Console.WriteLine($"Celsius to Fahrenheit: {TempConverter.CelsiusToFahrenheit(0)}");
